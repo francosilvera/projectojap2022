7dcdf62ef021d00d98d2f69c292c6c23466f636c
francosilvera <francosilvera25@gmail.com>
2022-08-12 19:03:40 -0300

    cambios en products

---
+++ js/products.js
@@ -0,0 +1,40 @@
+let categoriesArray = [];
+
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img scr="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.product.name +`</h4> 
+                        <p> `+ category.descripion +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.name + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+  
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
